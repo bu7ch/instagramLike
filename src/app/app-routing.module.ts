6c0ee00f427be4841251b3aa7007a231e034dd23
Rodolphe <dutchr@live.fr>
2019-09-06 16:24:04 +0200

    add login system and firebase configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,19 @@ import { AllPostsComponent } from './components/all-posts/all-posts.component';
 import { FollowingComponent } from './components/following/following.component';
 import { FavoritesComponent } from './components/favorites/favorites.component';
 import { HomeComponent } from './components/home/home.component';
-
+import { MyPostsComponent } from './components/my-posts/my-posts.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { LoginComponent } from './auth/login/login.component';
+import { CanActivateGuard } from "./auth/route-guard";
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
-  {path:'allposts', component: AllPostsComponent},
-  {path:'following', component: FollowingComponent},
-  {path:'favorites', component: FavoritesComponent},
-  {path:'MyPosts', component: AppComponent},
- 
+  {path:'allposts', component: AllPostsComponent, canActivate: [CanActivateGuard]},
+  {path:'following', component: FollowingComponent, canActivate: [CanActivateGuard]},
+  {path:'favorites', component: FavoritesComponent, canActivate: [CanActivateGuard]},
+  {path:'MyPosts', component: MyPostsComponent, canActivate: [CanActivateGuard]},
+  {path:'login', component: LoginComponent},
+  {path:'signup', component: SignUpComponent},
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'instagramLike';
+
+  ngOnInit() {
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyBhjwiwn_i0SKAgV0-VHH-q5mSzRiKiJo4",
+      authDomain: "instagramlike-a6c06.firebaseapp.com",
+      databaseURL: "https://instagramlike-a6c06.firebaseio.com",
+      projectId: "instagramlike-a6c06",
+      storageBucket: "instagramlike-a6c06.appspot.com",
+      messagingSenderId: "705324801301",
+      appId: "1:705324801301:web:25fb9604bb21228087dd1c"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
+
 }
